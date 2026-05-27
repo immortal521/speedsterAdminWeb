7317f467af6ad2747d4c0f9837a75177c1eb521c
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2026-05-27 21:38:08 +0800

    feat: build & ci

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,28 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://14.103.249.252:9527', // 后端接口地址
+        target: 'http://47.108.58.56:9527', // 后端接口地址
         changeOrigin: true,
 
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rolldownOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+        entryFileNames: 'assets/js/[name].[hash].js',
+        chunkFileNames: 'assets/js/[name].[hash].js',
+        assetFileNames: 'assets/[ext]/[name].[hash].[ext]',
+      },
+    },
+    outDir: 'speedsterDist',
+    sourcemap: true,
   },
 });
